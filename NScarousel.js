1cc543cba9eabcf450cd5b0e5bde1af357ccc1f8
Mauricio Soares <m.soares2603@gmail.com>
2014-12-16 18:22:37 -0200

    working on next, now cloning some nodes

---
+++ NScarousel.js
@@ -0,0 +1,39 @@
+(function(root) {
+  var NScarousel = function(container, configs) {
+    var configs = configs || {};
+
+    // set common variables
+    this.container = document.querySelector(container);
+    this.scrollable = this.container.getElementsByTagName('ul')[0];
+
+    this.configs = {
+      delay: configs.delay || '1s'
+    };
+
+    this.initialize();
+
+    return this;
+  };
+
+  NScarousel.prototype.initialize = function() {
+    this.setStyles();
+  };
+
+  NScarousel.prototype.setStyles = function() {
+    // set common styles to scrollable content
+    this.scrollable.style.width = '50000px'
+    this.scrollable.style.position = 'relative'
+    this.scrollable.style.transition = this.configs.delay
+    this.scrollable.style.left = '0'
+  };
+
+  NScarousel.prototype.next = function(e) {
+    if(e) e.preventDefault();
+
+    var clone = this.scrollable.children[0].cloneNode(true);
+    this.scrollable.appendChild(clone);
+    return this;
+  };
+
+  root.NScarousel = NScarousel;
+} (this))
